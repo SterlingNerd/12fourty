60d72e09c7c732581ba94b8928655e4c96eb7ba3
Josh Sterling <josh@sterling.digital>
2022-02-28 23:58:13 -0800

    Added Unit Tests to cover spec.

--- RefactorMoveFolderValidationTests/RefactorMoveFolderValidationTests.cs
+++ RefactorMoveFolderValidationTests/RefactorMoveFolderValidationTests.cs
@@ -1,5 +1,7 @@
 using System;
 
+using FluentAssertions;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
@@ -17,7 +19,7 @@ public class RefactorMoveFolderValidationTests
 	public void Init()
 	{
 		mockICustomReportFolderService = new Mock<ICustomReportFolderService>();
-		moveFolderValidation = new MoveFolderValidation(new User {Id = 1}, mockICustomReportFolderService.Object);
+		moveFolderValidation = new MoveFolderValidation(new User {Id = 1, RoleId = RoleId.Student}, mockICustomReportFolderService.Object);
 	}
 
 	#endregion
@@ -25,13 +27,82 @@ public class RefactorMoveFolderValidationTests
 	private Mock<ICustomReportFolderService> mockICustomReportFolderService;
 	private MoveFolderValidation moveFolderValidation;
 
+	// Any user may move their own folder item, but if moving to a non-shared folder, the user must own that folder
 	[TestMethod]
 	public void ValidateMoveFolderItem_OwnerMovingToOwnPrivateFolder_Success()
 	{
-		FolderItem folderItem = new FolderItem {OwnerId = 1};
+		FakeFolderItem folderItem = new() {OwnerId = 1};
 
-		mockICustomReportFolderService.Setup(x => x.Get(It.IsAny<long>())).Returns(new FolderItem {OwnerId = 1});
+		mockICustomReportFolderService.Setup(x => x.Get(It.IsAny<long>())).Returns(new FakeFolderItem {OwnerId = 1, IsShared = false});
 
-		Assert.IsTrue(moveFolderValidation.ValidateMoveFolderItem(folderItem, 10).IsSuccessful);
+		moveFolderValidation.ValidateMoveFolderItem(folderItem, 10)
+			.IsSuccessful
+			.Should()
+			.Be(true);
+	}
+
+	// * Shared item may only move into a shared folder
+	[TestMethod]
+	public void ValidateMoveFolderItem_OwnerMovingPrivateFolder_Invalid()
+	{
+		FakeFolderItem folderItem = new() {OwnerId = 1, IsShared = true};
+
+		mockICustomReportFolderService.Setup(x => x.Get(It.IsAny<long>())).Returns(new FakeFolderItem {OwnerId = 23, IsShared = false});
+
+		moveFolderValidation.ValidateMoveFolderItem(folderItem, 10)
+			.IsSuccessful
+			.Should()
+			.Be(false);
+	}
+
+	// * Only an admin can move another user's folder item, but only if both the folder item and the target folder are shared
+	[DataTestMethod]
+	[DataRow(false, false, false, false)]
+	[DataRow(false, false, true, false)]
+	[DataRow(false, true, false, false)]
+	[DataRow(false, true, true, false)]
+	[DataRow(true, false, false, false)]
+	[DataRow(true, false, true, false)]
+	[DataRow(true, true, false, false)]
+	[DataRow(true, true, true, true)]
+	public void ValidateMoveFolderItem_AdminMovingPrivateItem_Invalid(bool isAdmin, bool sourceShared, bool destinationShared, bool expected)
+	{
+		FakeFolderItem folderItem = new() {OwnerId = 1, IsShared = false};
+		moveFolderValidation = new MoveFolderValidation(new User {Id = 42, RoleId = isAdmin ? RoleId.Admin : RoleId.Student}, mockICustomReportFolderService.Object);
+
+		mockICustomReportFolderService.Setup(x => x.Get(It.IsAny<long>())).Returns(new FakeFolderItem {OwnerId = 23, IsShared = false});
+
+		moveFolderValidation.ValidateMoveFolderItem(folderItem, 10)
+			.IsSuccessful
+			.Should()
+			.Be(expected);
+	}
+
+	// * Root folder (i.e no targetFolderId) is always considered to be shared
+	[TestMethod]
+	public void ValidateMoveFolderItem_MoveToRootFolder_Success()
+	{
+		FakeFolderItem folderItem = new() {OwnerId = 1, IsShared = true};
+
+		//mockICustomReportFolderService.Setup(x => x.Get(0)).Returns(new FakeFolderItem {OwnerId = 23, IsShared = false});
+
+		moveFolderValidation.ValidateMoveFolderItem(folderItem, null)
+			.IsSuccessful
+			.Should()
+			.Be(true);
+	}
+
+	// Any user may move their own folder item
+	[TestMethod]
+	public void ValidateMoveFolderItem_OwnerMovingToOwnSharedFolder_Success()
+	{
+		FakeFolderItem folderItem = new() {OwnerId = 1};
+
+		mockICustomReportFolderService.Setup(x => x.Get(It.IsAny<long>())).Returns(new FakeFolderItem {OwnerId = 23, IsShared = true});
+
+		moveFolderValidation.ValidateMoveFolderItem(folderItem, 10)
+			.IsSuccessful
+			.Should()
+			.Be(true);
 	}
 }
